5c09354d82bf009167a33a60dddc0d10d998a01f
evingerbono <bono0823@gmail.com>
2024-02-12 13:56:16 +0100

    elhelyezés_a_mianben

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -3,7 +3,9 @@ package kiralynok;
 public class Kiralynok {
 
     public static void main(String[] args) {
-        
+        Tabla tabla = new Tabla('*');
+        tabla.Elhelyez(8);
+        tabla.Megjelenit();
     }
-    
+
 }
