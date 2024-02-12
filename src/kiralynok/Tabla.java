2069157806d9d7c387bd6d7701ffba40f6910a95
evingerbono <bono0823@gmail.com>
2024-02-12 13:44:44 +0100

    megjelenit_metódus

--- src/kiralynok/Tabla.java
+++ src/kiralynok/Tabla.java
@@ -14,4 +14,13 @@ public class Tabla {
             }
         }
     }
+
+    public void Megjelenit() {
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T[i].length; j++) {
+                System.out.print(T[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
