2ba540543f89cf885db2fe7b2c92f3c97e52bd34
evingerbono <bono0823@gmail.com>
2024-02-12 13:38:28 +0100

    konstruktor_létrehozás

--- src/kiralynok/Tabla.java
+++ src/kiralynok/Tabla.java
@@ -1,5 +1,17 @@
 package kiralynok;
 
 public class Tabla {
-    
+
+    private char[][] T;
+    private char uresCella;
+
+    public Tabla(char uresCella) {
+        T = new char[8][8];
+        this.uresCella = uresCella;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                T[i][j] = uresCella;
+            }
+        }
+    }
 }
